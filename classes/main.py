47be8464e1ccc73c24a75c1f8adaf0d57683b886
jorgen-kodehode <jorgen.kodehode@gmail.com>
2023-07-25 13:50:34 +0200

    changed output in main.py

--- classes/main.py
+++ classes/main.py
@@ -7,10 +7,9 @@ def main():
     soduku_solver.animate_ascii_board(board=soduku_solver.static_board)
     soduku_solver.solve_board(board=soduku_solver.static_board)
     print("_______________________")
+    soduku_solver.animate_ascii_board(board=soduku_solver.static_board)
     print("Solved in:")
     print("----%s seconds----" % (time.time() - start_time))
-    soduku_solver.animate_ascii_board(board=soduku_solver.static_board)
-
 
 
 
