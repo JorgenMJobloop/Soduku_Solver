11c5c431777409f762cfed13c3f02b407678b60d
jorgen-kodehode <jorgen.kodehode@gmail.com>
2023-07-25 13:43:22 +0200

    fixed recursive algorithm

--- classes/main.py
+++ classes/main.py
@@ -1,11 +1,15 @@
 #!/env/usr/bin/
 import soduku_solver
+import time
 
+start_time = time.time()
 def main():
-    print(soduku_solver.animate_ascii_board(board=any))
-    soduku_solver.solve_board(board=any)
-    print("______________")
-    soduku_solver.animate_ascii_board(board=any)
+    soduku_solver.animate_ascii_board(board=soduku_solver.static_board)
+    soduku_solver.solve_board(board=soduku_solver.static_board)
+    print("_______________________")
+    print("Solved in:")
+    print("----%s seconds----" % (time.time() - start_time))
+    soduku_solver.animate_ascii_board(board=soduku_solver.static_board)
 
 
 
--- classes/soduku_solver.py
+++ classes/soduku_solver.py
@@ -12,57 +12,59 @@ static_board = [[7,8,0,4,0,0,1,2,0],
 def animate_ascii_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
-            print("----------------")
-            for j in range(len(board[0])):
-                if j % 3 == 0 and j != 0:
-                    print(r" | ", end="")
-                if j == 8:
-                    print(board[i][j])
-                else:
-                    print(str(board[i][j]) + " ", end="")
+            print("------------------------")
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(r" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
 
 def find_empty_num(board):
     for i in range(len(board)):
-        for j in range(len(board[i])):
+        for j in range(len(board[0])):
             if board[i][j] == 0:
-                return (i, j) # return tuples of iterator i & j
+                return (i, j)  # return tuples of iterator i & j
     return None
 
-
-
-def solve_board(board, valid_num, pos):
-    find_num = find_empty_num(board=any)
+def solve_board(board):
+    find_num = find_empty_num(board)
     if not find_num:
         return True
     else:
         row, col = find_num
 
-    for i in range(1,10):
-        if valid_num(board, i, (col, row)):
+    for i in range(1, 10):
+        if valid_numbers(board, i, (row, col)):
             board[row][col] = i
-        
-        if solve_board(board):
-            return True
-        board[row][col] = 0
+
+            if solve_board(board):
+                return True
+
+            board[row][col] = 0
 
     return False
 
-def valid_numbers(board,num,pos):
+def valid_numbers(board, num, pos):
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
     for i in range(len(board)):
-        if board[pos[i][0]] == num and pos[1] != i:
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
-    for j in range(len(board)):
-        if board[pos[j][0]] == num and pos[1] != j:
-            return False
-        
+
     x_pos = pos[1] // 3
     y_pos = pos[0] // 3
 
-    for g in range(x_pos*3, y_pos*3 + 3):
-        for h in range(x_pos*3, y_pos*3 + 3):
-            if board[g][h] == num and (g,h) != pos:
+    for i in range(y_pos * 3, y_pos * 3 + 3):
+        for j in range(x_pos * 3, x_pos * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
                 return False
-    return True              
+    return True
 
-
-
+animate_ascii_board(static_board)
+solve_board(static_board)
+print("_______________________")
+animate_ascii_board(static_board)
